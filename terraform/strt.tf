6042eb88a49a476e0120881fec26b7c7e3215505
oddin_linux <npcodin@proton.me>
2025-11-14 17:08:33 +0100

    add some lines into strt.tf

--- terraform/strt.tf
+++ terraform/strt.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region = "north-1"
+  region = "eu-north-1"
 }
 
 resource "aws_instance" "terraform_instance" {
-  ami           = "ami-ami-0fa91bc90632c73c9" # AMI ID for Ubuntu in us-east-1
+  ami           = "ami-0fa91bc90632c73c9" # AMI ID for Ubuntu in us-east-1
   instance_type = "t3.micro"
+  #key_name      = "my-keypair"
 
   tags = {
     Name = "ItsUbuntuInstance"
