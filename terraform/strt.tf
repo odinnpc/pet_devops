cdccd97f793a5c0bec3ade28c6dee39f2fdbf85a
oddin_linux <npcodin@proton.me>
2025-11-14 18:53:08 +0100

    add new lines into strt.tf

--- terraform/strt.tf
+++ terraform/strt.tf
@@ -2,12 +2,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "terraform_instance" {
+resource "aws_instance" "ubuntu_with_terraform" {
+  #count = 3
   ami           = "ami-0fa91bc90632c73c9" # AMI ID for Ubuntu in us-east-1
   instance_type = "t3.micro"
-  #key_name      = "my-keypair"
+  key_name      = "super_duper"
+  vpc_security_group_ids = ["sg-03ad5e817c12f5faf"]
+
 
   tags = {
-    Name = "ItsUbuntuInstance"
+    Name = "instance_terraform_${count.index + 1}" # differentiate instances if count > 1
   }
 }
